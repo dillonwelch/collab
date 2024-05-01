46db2f5086c60c19ad234f3e711b850e6b030b30
Dillon Welch <daw0328@gmail.com>
2024-05-01 16:01:55 -0400

    Refactor to cache

--- lib/video_service.rb
+++ lib/video_service.rb
@@ -3,13 +3,22 @@ require "net/http"
 class VideoService
   def self.get
     # TODO: cache bust?
-    Rails.cache.fetch("video_service") do
+    # TODO: enable cache
+    # TODO: docs
+    # TODO: pagination
+    Rails.cache.fetch("video_service_get") do
       uri = URI("#{ENV['BASE_API_URL']}/videos")
       JSON.parse(Net::HTTP.get(uri))
     end
   end
 
   def self.get_by_video_id(video_id)
-    get["videos"].find { |hsh| hsh["video_id"] == video_id } || {}
+    # TODO: cache bust?
+    # TODO: enable cache
+    # TODO: docs
+    # TODO: pagination
+    Rails.cache.fetch("video_service_get_by_video_id") do
+      get["videos"].map { |hsh| [hsh["video_id"], hsh] }.to_h
+    end.fetch(video_id, {})
   end
 end
