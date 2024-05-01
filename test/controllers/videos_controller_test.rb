b387b5332e02dec78b35ac9e8c531de8e043902b
Dillon Welch <daw0328@gmail.com>
2024-05-01 12:59:37 -0400

    Wee

--- test/controllers/videos_controller_test.rb
+++ test/controllers/videos_controller_test.rb
@@ -6,14 +6,12 @@ class VideosControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  #         <div><img alt="Thumbnail image for video H1tQhK0n5Qk" src="https://i.ytimg.com/vi/H1tQhK0n5Qk/default.jpg" /></div>
-  #         <div>Title: thanks for 5 million</div>
-  #         <!--          TODO: add ...-->
-  #         <div>Description: #shorts</div>
-  #         <div>279357 views</div>
   test "video image thumbnail is displayed" do
     get root_path
-    assert_select "#H1tQhK0n5Qk>div", html: '<img alt="Thumbnail image for video H1tQhK0n5Qk" src="https://i.ytimg.com/vi/H1tQhK0n5Qk/default.jpg">'
+    assert_select(
+      "#H1tQhK0n5Qk>div",
+      html: '<img alt="Thumbnail image for video H1tQhK0n5Qk" src="https://i.ytimg.com/vi/H1tQhK0n5Qk/default.jpg">'
+    )
   end
 
   test "video title is displayed" do
@@ -21,6 +19,7 @@ class VideosControllerTest < ActionDispatch::IntegrationTest
     assert_select "#H1tQhK0n5Qk>div", "Title: thanks for 5 million"
   end
 
+  # TODO: test long description
   test "video description is displayed" do
     get root_path
     assert_select "#H1tQhK0n5Qk>div", "Description: #shorts"
