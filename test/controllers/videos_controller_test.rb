dc419cf9de4456531576ba5d477185f9d6c120a9
Dillon Welch <daw0328@gmail.com>
2024-05-01 12:47:26 -0400

    View testing for controller

--- test/controllers/videos_controller_test.rb
+++ test/controllers/videos_controller_test.rb
@@ -1,8 +1,28 @@
 require "test_helper"
 
 class VideosControllerTest < ActionDispatch::IntegrationTest
-  # test "should get get" do
-  #   get videos_get_url
-  #   assert_response :success
-  # end
+  test "index response should be a success" do
+    get root_path
+    assert_response :success
+  end
+
+  #         <div><img alt="Thumbnail image for video H1tQhK0n5Qk" src="https://i.ytimg.com/vi/H1tQhK0n5Qk/default.jpg" /></div>
+  #         <div>Title: thanks for 5 million</div>
+  #         <!--          TODO: add ...-->
+  #         <div>Description: #shorts</div>
+  #         <div>279357 views</div>
+  test "video title is displayed" do
+    get root_path
+    assert_select "#H1tQhK0n5Qk>div", "Title: thanks for 5 million"
+  end
+
+  test "video description is displayed" do
+    get root_path
+    assert_select "#H1tQhK0n5Qk>div", "Description: #shorts"
+  end
+
+  test "video views are displayed" do
+    get root_path
+    assert_select "#H1tQhK0n5Qk>div", "279357 views"
+  end
 end
