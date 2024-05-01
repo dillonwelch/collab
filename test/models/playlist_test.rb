14b4a9ae2e316fa1b03243cd5573a3f9238a78be
Dillon Welch <daw0328@gmail.com>
2024-05-01 11:50:48 -0400

    Tests

--- test/models/playlist_test.rb
+++ test/models/playlist_test.rb
@@ -6,4 +6,11 @@ class PlaylistTest < ActiveSupport::TestCase
     playlist.save
     assert_equal playlist.errors.messages[:name], ["can't be blank"]
   end
+
+  test "validates uniqueness of name" do
+    Playlist.create(name: "Good test playlist")
+    playlist = Playlist.new(name: "Good test playlist")
+    playlist.save
+    assert_equal playlist.errors.messages[:name], ["has already been taken"]
+  end
 end
