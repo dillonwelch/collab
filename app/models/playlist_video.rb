ca640150edf72b1dfd0f6a90a4483f7ce8a3c3f3
Dillon Welch <daw0328@gmail.com>
2024-05-01 17:58:58 -0400

    Tests for numericality

--- app/models/playlist_video.rb
+++ app/models/playlist_video.rb
@@ -5,12 +5,16 @@ class PlaylistVideo < ApplicationRecord
     self.position = (playlist.playlist_videos.maximum(:position) || 0) + 1
   end
 
+  # TODO: Validate existence.
   validates :video_id, presence: true
+
+  validates :position, numericality: { only_integer: true, greater_than: 0, allow_nil: true }
 
   # TODO: how to do validation with edits
   # validates :position, presence: true
 
-  # TODO: docs
+  # Fetches the JSON video information for the video ID assigned to the record.
+  # @return [Hash] JSON video information.
   def video
     VideoService.get_by_video_id(video_id)
   end
--- test/models/playlist_video_test.rb
+++ test/models/playlist_video_test.rb
@@ -3,12 +3,24 @@ require "test_helper"
 class PlaylistVideoTest < ActiveSupport::TestCase
   test "validates presence of playlist association" do
     video = PlaylistVideo.create(video_id: "123")
-    assert_equal video.errors.messages[:playlist], ["must exist"]
+    assert_equal ["must exist"], video.errors.messages[:playlist]
   end
 
   test "validates presence of video_id" do
     video = PlaylistVideo.create
-    assert_equal video.errors.messages[:video_id], ["can't be blank"]
+    assert_equal ["can't be blank"], video.errors.messages[:video_id]
+  end
+
+  test "validates position is an integer" do
+    video = PlaylistVideo.new(position: 1.1)
+    video.valid?
+    assert_equal ["must be an integer"], video.errors.messages[:position]
+  end
+
+  test "validates position is a positive number" do
+    video = PlaylistVideo.new(position: 0)
+    video.valid?
+    assert_equal ["must be greater than 0"], video.errors.messages[:position]
   end
 
   test "sets position to 1 on create if no videos are on the playlist" do
