f1702d8f8263e0b15a9960ba620f593f932878d6
Dillon Welch <daw0328@gmail.com>
2024-05-03 02:02:09 -0400

    Add cache busting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action do
+    VideoService.cache_bust if params[:cache_bust] == 'true'
+  end
 end
--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,6 +1,5 @@
 class VideosController < ApplicationController
   def index
-    # TODO: clear cache
     @result = VideoService.get
     @playlists = Playlist.all.pluck(:name, :id)
   end
